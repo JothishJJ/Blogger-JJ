39a2515e15af114836ac20fcc1553ea022a0069d
JothishJJ <jothishjj11@gmail.com>
2022-06-25 18:03:39 +0530

    Add Header Section

---
+++ components/HeadAFoot.tsx
@@ -0,0 +1,66 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+import React, { useState } from "react";
+import {
+  AppShell,
+  Header,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+  Text,
+} from "@mantine/core";
+
+import styles from "./HeadAFoot.module.css";
+
+const HeadAFoot: NextPage = (props: any) => {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+  return (
+    <AppShell
+      styles={{
+        main: {
+          background: "#fff",
+        },
+      }}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      fixed
+      header={
+        <Header height={70} p="md">
+          <div
+            style={{ display: "flex", alignItems: "center", height: "100%" }}
+          >
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+              <div>
+                <Burger
+                  opened={opened}
+                  onClick={() => setOpened((o) => !o)}
+                  size="sm"
+                  color={theme.colors.violet[3]}
+                  mr="xl"
+                />
+              </div>
+            </MediaQuery>
+            <Link href="/">
+              <a
+                className={styles.navTitle}
+                style={{ color: theme.colors.violet[0] }}
+              >
+                Blogger JJ
+              </a>
+            </Link>
+            <Text style={{ margin: "0 1rem" }}>|</Text>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </div>
+        </Header>
+      }
+    >
+      {/* Blank */}
+      {props.children}
+    </AppShell>
+  );
+};
+
+export default HeadAFoot;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,35 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
+import { MantineProvider } from "@mantine/core";
+
 // Components
-import Header from "../components/Header";
+import HeadAFoot from "../components/HeadAFoot";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <Head>
-        <title>Blogger JJ</title>
-        <meta
-          name="description"
-          content="This is a blog about coding, Web Developemnt and programming in general."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-    </div>
+    <MantineProvider
+      theme={{
+        colors: {
+          violet: ["#9053E0", "#BB9AE6", "#6F40AD", "#3E2461", "#4F4161"],
+        },
+      }}
+    >
+      <div>
+        <Head>
+          <title>Blogger JJ</title>
+          <meta
+            name="description"
+            content="This is a blog about coding, Web Developemnt and programming in general."
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        {/* @ts-ignore */}
+        <HeadAFoot>
+          <h1>This is a very cool website Guys! </h1>
+        </HeadAFoot>
+      </div>
+    </MantineProvider>
   );
 };
 
