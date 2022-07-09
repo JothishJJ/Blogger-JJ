7de0d8cd71a63d55e1edadb6157fe433a2007659
JothishJJ <jothishjj11@gmail.com>
2022-07-09 21:58:14 +0530

    Add components to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from "next";
 
-import { useMantineTheme } from "@mantine/core";
+import { useMantineTheme, Container, Avatar, Center } from "@mantine/core";
 
 import HeadAFoot from "../components/HeadAFoot";
+
+import styles from "./About.module.css";
 
 const About: NextPage = () => {
   const theme = useMantineTheme();
@@ -10,7 +12,24 @@ const About: NextPage = () => {
     <>
       {/* @ts-ignore */}
       <HeadAFoot>
-        <h1>About Me</h1>
+        <Container>
+          <h1 className={styles.heading}>About Me</h1>
+          <Center>
+            <Avatar
+              size={200}
+              alt="Profile Image"
+              src="https://pbs.twimg.com/profile_images/1341432402527145984/9vWLtNpz_400x400.jpg"
+              radius={100}
+            />
+          </Center>
+          <p className={styles.paragraph}>
+            Hi my name is Jothish JJ and I am the guy who is writing this blog.
+            I hope that by creating this blog I could improve my programming
+            skills and it could be a lot of fun as well. This blog will also
+            will be about tutorials on various things like Next Js which could
+            help you guys as well.
+          </p>
+        </Container>
       </HeadAFoot>
     </>
   );
